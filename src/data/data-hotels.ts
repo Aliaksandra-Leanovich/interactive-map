c3a7494dfeae6f75d0d05ef0b4be434361d981fa
Aliaksandra-Leanovich <aliaksandraleanovich@gmail.com>
2025-07-17 00:07:37 +0300

    feat: mockdata, types

---
+++ src/data/data-hotels.ts
@@ -0,0 +1,44 @@
+import { Hotel } from "@/types/hotel";
+
+export const hotels: Array<Hotel> = [
+  {
+    id: 1,
+    name: "Hotel Minsk",
+    address: "Пр-т Независимости, 11",
+    price: 120,
+    lat: 53.902284,
+    lng: 27.561831,
+  },
+  {
+    id: 2,
+    name: "President Hotel",
+    address: "ул. Кирова, 18",
+    price: 150,
+    lat: 53.895539,
+    lng: 27.553309,
+  },
+  {
+    id: 3,
+    name: "Victoria Hotel",
+    address: "пр-т Победителей, 59",
+    price: 110,
+    lat: 53.918847,
+    lng: 27.490101,
+  },
+  {
+    id: 4,
+    name: "DoubleTree by Hilton",
+    address: "ул. Петра Мстиславца 11",
+    price: 150,
+    lat: 53.9311,
+    lng: 27.6452,
+  },
+  {
+    id: 5,
+    name: "Green City Hotel",
+    address: "ул. Притыцкого 156",
+    price: 90,
+    lat: 53.9021,
+    lng: 27.4219,
+  },
+];
---
+++ src/types/hotel.ts
@@ -0,0 +1,8 @@
+export interface Hotel {
+  id: number;
+  name: string;
+  address: string;
+  price: number;
+  lat: number;
+  lng: number;
+}
